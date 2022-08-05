69c8aa24505387dfa3eb740a5dacde42711245c4
Dalian Grullon <dalian.business@aol.com>
2022-08-04 20:19:29 -0400

    Created Group Model

---
+++ models/Group.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Groups extends Model {}
+
+Groups.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    group_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'groups',
+  }
+);
+
+module.exports = Groups;
